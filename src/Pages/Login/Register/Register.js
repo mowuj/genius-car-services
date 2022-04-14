fb769c433be9b147adf3e2c63beef6aa5743681d
mowuj <mowuj92@gmail.com>
2022-04-14 16:45:18 +0600

    email verifycation

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import './Register.css'
 import auth from '../../../firebase.init';
 import SocialLogIn from '../SocialLogIn/SocialLogIn';
 const Register = () => {
+    const [agree,setAgree] =useState(false)
     const navigate = useNavigate();
     const [
     createUserWithEmailAndPassword,
     user,
     loading,
     error,
-    ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth,{sendEmailVerification:true});
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
     const navigateLogin = () => {
-        navigate('/login');
+        navigate('/home');
     }
     if (user) {
-        navigate('/')
+        console.log('user',user);
     }
-    const handleRegister = event => {
+    const handleRegister = async (event) => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(email,password)
-        createUserWithEmailAndPassword(email,password)
+        // const agree = event.target.terms.checked;
+        
+        await createUserWithEmailAndPassword(email,password)
+        await updateProfile({ displayName:name });
+        console.log('Updated profile');
+        navigate('/login');
+        
+        
     }
     return (
         <div className='register-form'>
@@ -34,9 +42,13 @@ const Register = () => {
                 <input type="email" name="email" id="" placeholder='Email Address' required/>
                 
                 <input type="password" name="password" id="" placeholder='Password'required/>
-                <input type="checkbox" name="terms" id="terms" />
-                <label htmlFor="terms">Accept Genius Car Terms and Condition</label>
-                <input className='btn btn-primary w-50 mx-auto mt-2' type="submit" value="Register" />
+                <input onClick={()=>setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+                {/* <label className={agree?'ps-2 text-primary':'ps-2 text-danger'} htmlFor="terms">Accept Genius Car Terms and Condition</label> */}
+                <label className={`ps-2 ${agree?'':'text-danger'}`} htmlFor="terms">Accept Genius Car Terms and Condition</label>
+                <input
+                    disabled={!agree}
+                    className='btn btn-primary w-50 mx-auto mt-2'
+                    type="submit" value="Register" />
             </form>
             <p>Already Have an account? <Link to='/login' className='text-primary pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
             <SocialLogIn></SocialLogIn>
